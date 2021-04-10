63c65394061f0f386b0a53fedbccc2029c37e70b
tetsu5555 <tetete1118@gmail.com>
2021-04-10 10:08:43 +0900

    use kv

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"kvshakyo/kv"
 )
 
 func main() {
 	fmt.Println("hello")
+
+	kv := kv.NewStore()
+	kv.Put("key", "value")
+
+	val := kv.Get("key")
+	fmt.Println(val)
 }
