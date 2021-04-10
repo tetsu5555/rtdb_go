c99bb737284a3896f88cc5b9071f6bdf59b8df43
tetsu5555 <tetete1118@gmail.com>
2021-04-10 10:44:13 +0900

    add CLI

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"kvshakyo/kv"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello")
+	s := *kv.NewStore()
 
-	kv := kv.NewStore()
-	kv.Put("key", "value")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Real time db")
+	flag := true
+	for flag {
+		fmt.Print("> ")
+		text, _ := reader.ReadString('\n')
+		input := strings.Split(strings.Trim(text, "\n"), " ")
+		switch input[0] {
+		case "GET":
+			key := input[1]
+			value := s.Get(key)
+			fmt.Println(value)
+		case "PUT":
+			key, value := input[1], input[2]
+			s.Put(key, value)
+			fmt.Println("Done...")
+		case "EXIT":
+			fmt.Println("EXIT...")
+			flag = false
+			break
+		}
 
-	val := kv.Get("key")
-	fmt.Println(val)
+	}
 }
